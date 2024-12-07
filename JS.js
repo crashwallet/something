e35e02ad742de34b4647b15d3af6c983193883d2
crashwallet <tolegenoverasyl1@gmail.com>
2024-12-07 23:00:27 +0500

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,8 @@
+const button1 = document.getElementById("btn1")
+const button2 = document.getElementById("btn2")
+button1.addEventListener("click", function(event){
+    alert("Оу, спасибо!")
+});
+button2.addEventListener("click", function(event) {
+    alert("Спасибо за положительный выбор!")
+})
